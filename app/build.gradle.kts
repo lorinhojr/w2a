7bae1a169236d9c537c73ff8fed14086fdd935ce
lorinhojr <lorinho13@live.com>
2026-01-10 00:33:51 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,5 +54,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation('androidx.webkit:webkit:1.9.0")
     implementation("com.google.android.material:material:1.11.0")
 }
