a132e1ef509804a92f3749bb9732739ef542b196
lorinhojr <lorinho13@live.com>
2026-01-10 01:10:12 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,45 +15,27 @@ android {
         versionName = "1.0"
     }
 
-    signingConfigs {
-        create("release") {
-            val storeFilePath = System.getenv("ORG_GRADLE_PROJECT_storeFile")
-            if (storeFilePath != null && storeFilePath.isNotBlank()) {
-                storeFile = file(storeFilePath)
-                storePassword = System.getenv("ORG_GRADLE_PROJECT_storePassword")
-                keyAlias = System.getenv("ORG_GRADLE_PROJECT_keyAlias")
-                keyPassword = System.getenv("ORG_GRADLE_PROJECT_keyPassword")
-            }
-        }
-    }
-
     buildTypes {
         release {
             isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
-        debug {
-            isMinifyEnabled = false
-        }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation('androidx.webkit:webkit:1.9.0")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+
+    // ESSENCIAL PARA O CONSTRUCT 3
+    implementation("androidx.webkit:webkit:1.12.1")
 }
